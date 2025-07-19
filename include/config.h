7e11326747b2612ff12df814678f627dff282987
Conor Rooney <178730048+ConorRooney24@users.noreply.github.com>
2025-07-19 17:31:37 +0100

    Implement config.h with basic settings

---
+++ include/config.h
@@ -0,0 +1,19 @@
+// config.h
+// Stores settings that are taken in by the parser and used to generate the word search.
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+typedef struct
+{
+	int grid_width = 20;
+	int grid_height = 20;
+
+	int output_to_file = 0;
+	char *output_file_name = "word_search.txt";
+
+	int num_to_generate = 1;
+
+} Config;
+
+#endif
