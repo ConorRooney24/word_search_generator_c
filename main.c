3d71d60550bfb6189d0327ea56ec1a0255f3b92d
Conor Rooney <178730048+ConorRooney24@users.noreply.github.com>
2025-07-19 17:37:24 +0100

    add basic structure to main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "include/config.h"
+#include "include/grid.h"
+#include "include/parser.h"
+#include "include/random_numbers.h"
+
+int main (int argc, char *argv[])
+{
+	
+        return 0;
+}
