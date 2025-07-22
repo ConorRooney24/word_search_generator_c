2cdf4bcb4f5e4bb9762a5fb8361274cc747320ea
Conor Rooney <178730048+ConorRooney24@users.noreply.github.com>
2025-07-22 21:45:56 +0100

    Add detail to '--help' usage menu in parser.c and change default values in config.c.

--- src/config.c
+++ src/config.c
@@ -7,8 +7,8 @@ Config config;
 
 void config_assign_defaults(Config *config)
 {
-        config->grid_width = 20;
-        config->grid_height = 20;
+        config->grid_width = 30;
+        config->grid_height = 30;
         config->output_to_file = 0;
         config->output_file_name = strdup("word_search.txt");
         config->puzzles_to_generate = 1;
--- src/parser.c
+++ src/parser.c
@@ -29,7 +29,24 @@ int parse_argument(int argc, char *argv[], int index)
 {
         if (strcmp(argv[index], "--help") == 0 )
         {
+                printf("Word_Search_Generator_C\n\n");
                 printf("Usage: wsgen [OPTIONS]\n\n");
+                printf("Options:\n");
+                printf("   -h, --height <n>          Set the height of the puzzle grid.     (Default: 30)\n\n");
+                printf("   -w, --width <n>           Set the width of the puzzle grid.      (Default: 30)\n\n");
+                printf("   -n, --num-puzzles <n>     Set the number of puzzles to output.   (Default: 1)\n\n");
+                printf("   -o, --output <filename>   Enables file output. A filename is     (Default: Disabled,\n");
+                printf("                             Optional. If no filename is             Default Filename: 'word_search.txt')\n");
+                printf("                             specified, the default filename will\n");
+                printf("                             be used.\n\n");
+                printf("Example Usage:\n");
+                printf("   wsgen -w 50 -h 50 -o                              (This would output a 50 by 50 puzzle with file\n");
+                printf("                                                      output enabled. The output file name would be the\n");
+                printf("                                                      default 'word_search.txt')\n\n");
+                printf("   wsgen --width 40 --height 40 -n 3 -o puzzle.txt   (This would output 3 unique 40 by 40 puzzles. The\n");
+                printf("                                                      three puzzles would be stored in a single file\n");
+                printf("                                                      called 'puzzle.txt')\n");
+
                 return 1; // Print usage menu and exit if --help is detected.
         }
         else
